9d4a3cb5338a96508e9061bc5f6cb88d9c9d00d4
parbez <imranbarbhuiya.fsd@gmail.com>
2025-08-10 17:22:48 +0530

    Im batman

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import common from 'eslint-config-mahir/common';
+import edge from 'eslint-config-mahir/edge';
+import module from 'eslint-config-mahir/module';
+import node from 'eslint-config-mahir/node';
+import react from 'eslint-config-mahir/react';
+import typescript from 'eslint-config-mahir/typescript';
+
+/**
+ * @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray}
+ */
+export default [
+	...common,
+	...node,
+	...typescript,
+	...module,
+	...react,
+	...edge,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ['eslint.config.js'],
+					defaultProject: 'tsconfig.app.json',
+				},
+			},
+		},
+	},
+	{
+		ignores: ['.github', 'dist', 'src/vite-env.d.ts'],
+	},
+];
